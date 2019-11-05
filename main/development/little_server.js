13f46d7d009bb923a7d1a4d0ddfbe49da2d9fe98
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-05 15:04:41 +0100

    updated little_server

--- main/development/little_server.js
+++ main/development/little_server.js
@@ -3,12 +3,28 @@ const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
 
+html = `
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head>
+	<script src="../node_modules/d3/dist/d3.min.js"></script>
+</head>
+<body>
+	<script src="../saD3.js" type="text/JavaScript"></script>
+</body>
+`
+
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
 
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  //res.setHeader('Content-Type', 'text/plain');
+  //res.render('index', { title: 'development' });
+  //res.end();
+
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.write(html);
   res.end();
 });
 
