b1eb5f749e4c84749f107e2c9ea00bc7b4a35eaa
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-07 09:56:25 +0100

    removed bloated dependencies from main.js in require

--- main/development/requireJS_framework/www/app/main.js
+++ main/development/requireJS_framework/www/app/main.js
@@ -2,11 +2,11 @@ define(function (require) {
     // Load any app-specific modules
     // with a relative require call,
     // like:
-    var messages = require('./messages');
+    //var messages = require('./messages');
 
     // Load library/vendor modules using
     // full IDs, like:
-    var print = require('print');
+    //var print = require('print');
 
     print(messages.getHello());
 
