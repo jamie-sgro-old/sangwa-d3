9506a7dd2c7d44709e8829275d7c93e02b0c3974
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-07 10:05:30 +0100

    added test.js for data input and execution

---
+++ main/development/src/test.js
@@ -0,0 +1,25 @@
+var running = true;
+
+if (running) {
+  b = new BarGraph(
+    960,
+    500,
+    {top: 10, right: 30, bottom: 30, left: 30},
+    {top: "rgb(237, 85, 101)", bottom: "rgb(255, 255, 255)"},
+  );
+
+  dataDate = [
+    {start_date: "2004-04-10", value: 2},
+    {start_date: "2004-04-11", value: 2},
+    {start_date: "2004-04-12", value: 2},
+    {start_date: "2004-04-13", value: 2},
+    {start_date: "2004-04-14", value: 2},
+    {start_date: "2004-04-15", value: 2},
+    {start_date: "2004-04-16", value: 6},
+    {start_date: "2004-04-17", value: 2},
+    {start_date: "2004-04-18", value: 10},
+    {start_date: "2004-04-19", value: 8},
+  ]
+
+  b.plot(dataDate);
+}
