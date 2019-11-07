4e77d41d28e73434a3500dd6acdfdd625d3e484a
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-07 10:27:50 +0100

    removed comments

--- main/development/src/main.js
+++ main/development/src/main.js
@@ -1,15 +1,4 @@
 define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    //var messages = require('./messages');
-
-    // Load library/vendor modules using
-    // full IDs, like:
-    //var print = require('print');
-
-    //print(messages.getHello());
-
     require("./Base_D3.js");
     require("./Histogram.js");
     require("./Bargraph.js");
