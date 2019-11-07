031f1515388f8e0b6cdf7a15c1719af407fba986
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-07 12:54:40 +0100

    moved obj into parameter for getBins

--- main/development/src/main.js
+++ main/development/src/main.js
@@ -2,5 +2,6 @@ define(function (require) {
     require("./Base_D3.js");
     require("./Histogram.js");
     require("./Bargraph.js");
+    //require("./gallerry-int-hist.js");
     require("./test.js");
 });
