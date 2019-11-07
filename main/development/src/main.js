d0fc68134fb584883a9f2495128c62b53ad89680
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-07 14:09:18 +0100

    removed outdated js file from main.js

--- main/development/src/main.js
+++ main/development/src/main.js
@@ -2,6 +2,5 @@ define(function (require) {
     require("./Base_D3.js");
     require("./Histogram.js");
     require("./Bargraph.js");
-    require("./gallery-int-hist.js");
     require("./test.js");
 });
