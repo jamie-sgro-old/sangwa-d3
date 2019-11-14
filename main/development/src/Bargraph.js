61e02c295b0dfa3ef3ee012a8867b82051e40732
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-14 14:52:08 +0100

    removed date related function to Bargraph class

--- main/development/src/Bargraph.js
+++ main/development/src/Bargraph.js
@@ -69,21 +69,8 @@ class Bargraph extends Base_D3 {
 
 
 
-  /**
-  * Map dates of any range to a particular pixel point on an svg element
-  *
-  * path.data() indicates all the data in an array of JSON objects
-  * d in the function indicates a single datapoint in path.data()
-  *
-  * @param {obj} data - reference to the data from d3 object calling the function
-  *
-  */
-  getWidthScale(domain) {
-    return d3.scaleTime()
-      .domain(domain)
-      .range([0, this.width])
-      .nice();
-  };
+  /** Polymorphism */
+  getWidthScale() {};
 
 
 
@@ -156,26 +143,6 @@ class Bargraph extends Base_D3 {
   };
   _getAttr_r(path, obj) {
     path.attr("r", obj.heightScale.bandwidth()/2);
-  };
-
-
-
-  /**
-   * parseRawData - pre clean raw data in the form of a string that matches the date
-   * string provided
-   *
-   * @param  {array} rawData an array of json objects
-   * @return {array}         an array of parsed json objects according to
-   *  d3.timeParse
-   */
-  parseRawData(obj, rawData) {
-    var parseTime = d3.timeParse("%Y-%m-%d");
-    return rawData.map(function(d, i) {
-      return {
-        [obj.xLabel]: parseTime(d[obj.xLabel]),
-        [obj.yLabel]: d[obj.yLabel]
-      };
-    });
   };
 
 
