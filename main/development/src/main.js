eeef1c7953e3a0c40b698059d79959f52cb01dd6
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-14 13:37:00 +0100

    added Motion_D3 class to main.js

--- main/development/src/main.js
+++ main/development/src/main.js
@@ -1,5 +1,6 @@
 define(function (require) {
     require("./Base_D3.js");
+    require("./Motion_D3.js");
     require("./Histogram.js");
     require("./Histogram_Int.js");
     require("./Histogram_Date.js");
