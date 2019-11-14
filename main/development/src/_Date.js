6335cb7bd077e6e67d01039eda70a641d1d16d9c
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-14 14:29:42 +0100

    added _Date class file

---
+++ main/development/src/_Date.js
@@ -0,0 +1,47 @@
+/**
+ * Extention of Data_D3 for data that is of type: string
+ * @class
+ *
+ * Currently, date strings are expected to match the d3.timeParse "%Y-%m-%d"
+ * i.e. "2004-04-15"
+ */
+class _Date {
+  /** @constructor */
+  constructor() {
+    
+  };
+
+
+  /**
+  * Map dates of any range to a particular pixel point on an svg element
+  *
+  * path.data() indicates all the data in an array of JSON objects
+  * d in the function indicates a single datapoint in path.data()
+  *
+  * @param {obj} data - reference to the data from d3 object calling the function
+  *
+  */
+  getWidthScale(domain) {
+    return d3.scaleTime()
+      .domain(domain)
+      .rangeRound([0, this.width])
+      .nice();
+  };
+
+
+
+  /**
+   * parseRawData - pre clean raw data in the form of a string that matches the date
+   * string provided
+   *
+   * @param  {array} rawData an array of json objects
+   * @return {array}         an array of parsed json objects according to
+   *  d3.timeParse
+   */
+  parseRawData(obj, rawData) {
+    var parseTime = d3.timeParse("%Y-%m-%d");
+    return rawData.map(function(d, i) {
+      return parseTime(d[obj.yLabel]);
+    });
+  };
+}; // End Class
