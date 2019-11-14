3cf49f5c9e0962c797ff6eb2fe4783301b0e8378
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-14 14:29:33 +0100

    added _Date class

--- main/development/src/main.js
+++ main/development/src/main.js
@@ -1,6 +1,7 @@
 define(function (require) {
     require("./Base_D3.js");
     require("./Motion_D3.js");
+    require("./_Date.js");
     require("./Histogram.js");
     require("./Histogram_Int.js");
     require("./Histogram_Date.js");
