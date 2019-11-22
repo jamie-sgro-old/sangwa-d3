0680aa66ac266a98434933600ea9f7989f6995b8
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-22 16:19:34 +0100

    rotated pubBtn

--- main/development/src/_D3/Pub_D3.js
+++ main/development/src/_D3/Pub_D3.js
@@ -13,21 +13,24 @@ class Pub_D3 {
    * makePubBtn - Create an interactable button in the svg of the graph (canvas)
    * that downloads a .png file to the local machine of the id element of the svg
    *
+   * class = menu means it's part of a user interface
+   *
    * @return {type}  returns nothing
    */
   makePubBtn() {
     var obj = this;
-    var unit = 50;
+    var unit = 70;
     var alpha = "0.2"
 
     this.canvas
       .append("g")
-        .attr("class", "pub")
+        .attr("class", "menu")
         .append("rect")
-        .attr("x", obj.width - unit)
-        .attr("y", 0)
+        .attr("x", obj.width + obj.margin.right - (unit/2))
+        .attr("y", 0 - obj.margin.top - (unit/2))
         .attr("width", unit)
         .attr("height", unit)
+        .attr("transform", "rotate(45, " + (obj.width + obj.margin.right) + ", " + (0 - obj.margin.top) + ")")
         .attr("fill", "rgba(0,0,0," + alpha + ")")
         .style("cursor", "pointer")
         .on("mouseover", function() {
@@ -38,6 +41,8 @@ class Pub_D3 {
         })
         .on("click", function() {
           d3.select(this).attr("fill", "rgba(0,0,0,0)")
+
+          document.getElementById(obj.id).getElementsByClassName("pub")
 
           saveSvgAsPng(
             document.getElementById(obj.id),
