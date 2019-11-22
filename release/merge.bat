1ccfe8ca405958fcfa11bb4e7ae53dacd5c3ecc9
jamie-sgro <jamie.sgro@uwaterloo.ca>
2019-11-22 09:33:56 +0100

    added merge file

---
+++ release/merge.bat
@@ -0,0 +1,17 @@
+cd %cd%
+cd ../main
+copy "development/\src/\_D3/\Base_D3.js"^
+ + "development/\src/\_D3/\Motion_D3.js"^
+ + "development/\src/\_D3/\Colour_D3.js"^
+ + "development/\src/\_D3/\Pub_D3.js"^
+ + "development/\src/\_Int.js"^
+ + "development/\src/\_Date.js"^
+ + "development/\src/\Histogram/\Histogram.js"^
+ + "development/\src/\Histogram/\Histogram_Int.js"^
+ + "development/\src/\Histogram/\Histogram_Date.js"^
+ + "development/\src/\Bargraph/\Bargraph.js"^
+ + "development/\src/\Bargraph/\Bargraph_Date.js" concat.txt
+pause
+
+
+
